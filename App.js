b5613c7ba09f3fb616a9817cf66bc63bc6189d22
Marion <marion.carambula@modop.com>
2018-09-04 15:35:29 -0500

    feat
    Adding TabBar

--- App.js
+++ App.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Platform, StatusBar } from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { TabNavigator } from 'react-navigation';
+import { Constants } from 'expo';
 import AddEntry from './components/AddEntry';
 import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import { purple, white } from './utils/colors';
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
+}
+
+const Tabs = TabNavigator({
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+        },
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+        },
+      },
+    },
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? white : purple,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
+        }
+    }
+)
 
 export default class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducer)}>
                 <View style={{flex: 1}}>
-                    <History />
+                    <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+                    <Tabs />
                 </View>
             </Provider>
         )
